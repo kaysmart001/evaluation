34b86605054afec711d4817bd6e604b25351724e
kaysmart001 <luwoyekayode99@gmail.com>
2023-01-30 10:51:28 +0100

    Update connect_db.php

--- scripts/connect_db.php
+++ scripts/connect_db.php
@@ -1,12 +1,12 @@
 <?php 
 
-$db_host = "localhost";
+$db_host = "sql311.epizy.com";
 // Place the username for the MySQL database here
-$db_username = "root"; 
+$db_username = "epiz_33459705"; 
 // Place the password for the MySQL database here
-$db_pass = ""; 
+$db_pass = "hvZgW4p2guu7"; 
 // Place the name for the MySQL database here
-$db_name = "mytesting_db";
+$db_name = "epiz_33459705_mytest_db";
 
 // Run the connection here 
 mysql_connect("$db_host","$db_username","$db_pass") or die (mysql_error());
